d6c5d1c6e3d7b4febcfdf3e739c71166517744e0
Kenneth Myhra <kennethmyhra@gmail.com>
2018-07-06 12:47:36 +0200

    Missing file commit 40901931aa

---
+++ src/fhir-tool/Extensions/StringExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace FhirTool.Extensions
+{
+    internal static class StringExtensionMethods
+    {
+        internal static string UpperCaseFirstCharacter(this string s)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+            return $"{s.Substring(0, 1).ToUpper()}{s.Substring(1)}";
+        }
+    }
+}
