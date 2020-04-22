a215bc88206e57d45bd5e4af3b91270f9398833f
Kenneth Myhra <kennethmyhra@gmail.com>
2020-04-22 21:32:26 +0200

    Refactored VerifyItemValidation
    * Also removed a condition that made VerifyItemValidation only run for
      items of string and text

--- src/fhir-tool/Extensions/QuestionnaireExtensions.cs
+++ src/fhir-tool/Extensions/QuestionnaireExtensions.cs
@@ -18,5 +18,144 @@ namespace FhirTool.Extensions
 
             return false;
         }
+        
+        public static IEnumerable<MissingValidationIssue> VerifyItemValidation(this Questionnaire questionnaire, Questionnaire.ItemComponent item)
+        {
+            var issues = new List<MissingValidationIssue>();
+
+            issues.AddRange(VerifyStringAndTextValidation(item));
+            issues.AddRange(VerifyRepeatingItemValidation(item));
+            issues.AddRange(VerifyMaxOrMinValueIsSet(item));
+            issues.AddRange(VerifyAttachmentValidation(item));
+
+            foreach (Questionnaire.ItemComponent itm in item.Item)
+            {
+                issues.AddRange(questionnaire.VerifyItemValidation(itm));
+            }
+
+            return issues;
+        }
+
+        private static IEnumerable<MissingValidationIssue> VerifyAttachmentValidation(Questionnaire.ItemComponent item)
+        {
+            if (item.Type != Questionnaire.QuestionnaireItemType.Attachment) return new MissingValidationIssue[0];
+
+            var issues = new List<MissingValidationIssue>();
+
+            if (item.GetExtensions(Constants.QuestionnaireAttachmentMaxSize).Count() == 0)
+            {
+                issues.Add(new MissingValidationIssue
+                {
+                    LinkId = item.LinkId,
+                    Severity = MissingValidationSeverityEnum.Warning,
+                    Details = $"An item of type '{item.Type}' is missing the 'maxSize' attribute. Consider setting the 'maxSize' attribute."
+                });
+            }
+
+            return issues;
+        }
+
+        private static IEnumerable<MissingValidationIssue> VerifyMaxOrMinValueIsSet(Questionnaire.ItemComponent item)
+        {
+            if (!(item.Type == Questionnaire.QuestionnaireItemType.Date
+                || item.Type == Questionnaire.QuestionnaireItemType.DateTime
+                || item.Type == Questionnaire.QuestionnaireItemType.Time
+                || item.Type == Questionnaire.QuestionnaireItemType.Integer
+                || item.Type == Questionnaire.QuestionnaireItemType.Decimal))
+            {
+                return new MissingValidationIssue[0];
+            }
+
+            var issues = new List<MissingValidationIssue>();
+            if (item.GetExtensions(Constants.MaxValueUri).Count() == 0 || item.GetExtensions(Constants.MinValueUri).Count() == 0)
+            {
+                issues.Add(new MissingValidationIssue
+                {
+                    LinkId = item.LinkId,
+                    Severity = MissingValidationSeverityEnum.Information,
+                    Details = $"Consider setting the 'maxValue' and 'minValue' attribute for items of type '{item.Type}'."
+                });
+            }
+
+            return issues;
+        }
+
+        private static IEnumerable<MissingValidationIssue> VerifyRepeatingItemValidation(Questionnaire.ItemComponent item)
+        {
+            var issues = new List<MissingValidationIssue>();
+            if (item.Repeats == true)
+            {
+                if (item.GetExtensions(Constants.MaxOccursUri).Count() == 0)
+                {
+                    issues.Add(new MissingValidationIssue
+                    {
+                        LinkId = item.LinkId,
+                        Severity = MissingValidationSeverityEnum.Warning,
+                        Details = $"An item where the attribute 'repeats' is set to 'true' a 'maxOccurs' is recommended to be set."
+                    });
+                }
+            }
+
+            return issues;
+        }
+
+        private static IEnumerable<MissingValidationIssue> VerifyStringAndTextValidation(Questionnaire.ItemComponent item)
+        {
+            if ((item.Type != Questionnaire.QuestionnaireItemType.String && item.Type != Questionnaire.QuestionnaireItemType.Text)
+                || !item.IsItemControlOfType("help", "help-link", "inline"))
+            {
+                return new MissingValidationIssue[0];
+            }
+
+            var issues = new List<MissingValidationIssue>();
+            // Issues an error if a maxLength has NOT been set
+            if (!item.MaxLength.HasValue)
+            {
+                issues.Add(new MissingValidationIssue
+                {
+                    LinkId = item.LinkId,
+                    Severity = MissingValidationSeverityEnum.Error,
+                    Details = $"An item of type '{item.Type}' must have the 'maxLength' attribute set."
+                });
+            }
+            // Issues an error if a regex has NOT been set
+            if (item.GetExtensions(Constants.RegexUri).Count() == 0)
+            {
+                issues.Add(new MissingValidationIssue
+                {
+                    LinkId = item.LinkId,
+                    Severity = MissingValidationSeverityEnum.Error,
+                    Details = $"An item of type '{item.Type}' must have the 'regex' attribute set."
+                });
+            }
+            if (item.Type == Questionnaire.QuestionnaireItemType.String)
+            {
+                // Issues an error for ItemType == 'string' if a maxLength of no more than 250 characters has been set.
+                if (item.MaxLength.HasValue && item.MaxLength.Value > 250)
+                {
+                    issues.Add(new MissingValidationIssue
+                    {
+                        LinkId = item.LinkId,
+                        Severity = MissingValidationSeverityEnum.Error,
+                        Details = $"An item of type '{item.Type}' must have a 'maxLength' of no more than 250 characters."
+                    });
+                }
+            }
+            if (item.Type == Questionnaire.QuestionnaireItemType.Text)
+            {
+                // Issues a warning for ItemType == 'text' if a maxLength of more than 2500 characters has been set.
+                if (item.MaxLength.HasValue && item.MaxLength.Value > 2500)
+                {
+                    issues.Add(new MissingValidationIssue
+                    {
+                        LinkId = item.LinkId,
+                        Severity = MissingValidationSeverityEnum.Warning,
+                        Details = $"Item field of '{item.Type}' has a 'maxLength' of more than 2500 characters. This is allowed, but not recommended."
+                    });
+                }
+            }
+
+            return issues;
+        }
     }
 }
--- src/fhir-tool/Program.cs
+++ src/fhir-tool/Program.cs
@@ -182,7 +182,7 @@ namespace FhirTool
                 return false;
             }
 
-            IEnumerable<MissingValidationIssue> issues = VerifyItemValidation(questionnaire.Item);
+            IEnumerable<MissingValidationIssue> issues = VerifyItemValidation(questionnaire);
 
             foreach (MissingValidationIssue issue in issues)
             {
@@ -206,151 +206,12 @@ namespace FhirTool
             return !issues.Any(i => i.Severity == MissingValidationSeverityEnum.Error);
         }
 
-        private static IEnumerable<MissingValidationIssue> VerifyItemValidation(IEnumerable<Questionnaire.ItemComponent> items)
+        private static IEnumerable<MissingValidationIssue> VerifyItemValidation(Questionnaire questionnaire)
         {
             var issues = new List<MissingValidationIssue>();
-            foreach(Questionnaire.ItemComponent item in items)
+            foreach(Questionnaire.ItemComponent item in questionnaire.Item)
             {
-                issues.AddRange(VerifyItemValidation(item));
-            }
-
-            return issues;
-        }
-
-        private static IEnumerable<MissingValidationIssue> VerifyItemValidation(Questionnaire.ItemComponent item)
-        {
-            var issues = new List<MissingValidationIssue>();
-
-            if((item.Type == Questionnaire.QuestionnaireItemType.String || item.Type == Questionnaire.QuestionnaireItemType.Text)
-                && !item.IsItemControlOfType("help", "help-link", "inline"))
-            {
-                issues.AddRange(VerifyStringAndTextValidation(item));
-                issues.AddRange(VerifyRepeatingItemValidation(item));
-                issues.AddRange(VerifyMaxOrMinValueIsSet(item));
-                issues.AddRange(VerifyAttachmentValidation(item));
-            }
-
-            foreach(Questionnaire.ItemComponent itm in item.Item)
-            {
-                issues.AddRange(VerifyItemValidation(itm));
-            }
-
-            return issues;
-        }
-
-        private static IEnumerable<MissingValidationIssue> VerifyAttachmentValidation(Questionnaire.ItemComponent item)
-        {
-            if (item.Type != Questionnaire.QuestionnaireItemType.Attachment) return new MissingValidationIssue[0];
-
-            var issues = new List<MissingValidationIssue>();
-
-            if (item.GetExtensions(Constants.QuestionnaireAttachmentMaxSize).Count() == 0)
-            {
-                issues.Add(new MissingValidationIssue
-                {
-                    LinkId = item.LinkId,
-                    Severity = MissingValidationSeverityEnum.Warning,
-                    Details = $"An item of type '{item.Type}' is missing the 'maxSize' attribute. Consider setting the 'maxSize' attribute."
-                });
-            }
-
-            return issues;
-        }
-
-        private static IEnumerable<MissingValidationIssue> VerifyMaxOrMinValueIsSet(Questionnaire.ItemComponent item)
-        {
-            if(!(item.Type == Questionnaire.QuestionnaireItemType.Date
-                || item.Type == Questionnaire.QuestionnaireItemType.DateTime
-                || item.Type == Questionnaire.QuestionnaireItemType.Time
-                || item.Type == Questionnaire.QuestionnaireItemType.Integer
-                || item.Type == Questionnaire.QuestionnaireItemType.Decimal))
-            {
-                return new MissingValidationIssue[0];
-            }
-
-            var issues = new List<MissingValidationIssue>();
-            if(item.GetExtensions(Constants.MaxValueUri).Count() == 0 || item.GetExtensions(Constants.MinValueUri).Count() == 0)
-            {
-                issues.Add(new MissingValidationIssue
-                {
-                    LinkId = item.LinkId,
-                    Severity = MissingValidationSeverityEnum.Information,
-                    Details = $"Consider setting the 'maxValue' and 'minValue' attribute for items of type '{item.Type}'."
-                });
-            }
-
-            return issues;
-        }
-
-        private static IEnumerable<MissingValidationIssue> VerifyRepeatingItemValidation(Questionnaire.ItemComponent item)
-        {
-            var issues = new List<MissingValidationIssue>();
-            if (item.Repeats == true)
-            {
-                if (item.GetExtensions(Constants.MaxOccursUri).Count() == 0)
-                {
-                    issues.Add(new MissingValidationIssue
-                    {
-                        LinkId = item.LinkId,
-                        Severity = MissingValidationSeverityEnum.Warning,
-                        Details = $"An item where the attribute 'repeats' is set to 'true' a 'maxOccurs' is recommended to be set."
-                    });
-                }
-            }
-
-            return issues;
-        }
-
-        private static IEnumerable<MissingValidationIssue> VerifyStringAndTextValidation(Questionnaire.ItemComponent item)
-        {
-            if (item.Type != Questionnaire.QuestionnaireItemType.String && item.Type != Questionnaire.QuestionnaireItemType.Text) return new MissingValidationIssue[0];
-
-            var issues = new List<MissingValidationIssue>();
-            // Issues an error if a maxLength has NOT been set
-            if (!item.MaxLength.HasValue)
-            {
-                issues.Add(new MissingValidationIssue
-                {
-                    LinkId = item.LinkId,
-                    Severity = MissingValidationSeverityEnum.Error,
-                    Details = $"An item of type '{item.Type}' must have the 'maxLength' attribute set."
-                });
-            }
-            // Issues an error if a regex has NOT been set
-            if (item.GetExtensions(Constants.RegexUri).Count() == 0)
-            {
-                issues.Add(new MissingValidationIssue
-                {
-                    LinkId = item.LinkId,
-                    Severity = MissingValidationSeverityEnum.Error,
-                    Details = $"An item of type '{item.Type}' must have the 'regex' attribute set."
-                });
-            }
-            if (item.Type == Questionnaire.QuestionnaireItemType.String)
-            {
-                // Issues an error for ItemType == 'string' if a maxLength of no more than 250 characters has been set.
-                if (item.MaxLength.HasValue && item.MaxLength.Value > 250)
-                {
-                    issues.Add(new MissingValidationIssue
-                    {
-                        LinkId = item.LinkId,
-                        Severity = MissingValidationSeverityEnum.Error,
-                        Details = $"An item of type '{item.Type}' must have a 'maxLength' of no more than 250 characters."
-                    });
-                }
-            }
-            if (item.Type == Questionnaire.QuestionnaireItemType.Text)
-            {
-                // Issues a warning for ItemType == 'text' if a maxLength of more than 2500 characters has been set.
-                if (item.MaxLength.HasValue && item.MaxLength.Value > 2500)
-                {
-                    issues.Add(new MissingValidationIssue
-                    {
-                        LinkId = item.LinkId,
-                        Severity = MissingValidationSeverityEnum.Warning,
-                        Details = $"Item field of '{item.Type}' has a 'maxLength' of more than 2500 characters. This is allowed, but not recommended."
-                    });
-                }
+                issues.AddRange(questionnaire.VerifyItemValidation(item));
             }
 
             return issues;
