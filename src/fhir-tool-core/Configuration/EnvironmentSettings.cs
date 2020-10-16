3944f4c3e4fa0a0ce22e1191801ba810892a2a41
Kenneth Myhra <kennethmyhra@gmail.com>
2020-10-16 10:23:52 +0200

    refactor: Refactored Environment Settings (#62)
    * Replaced dependency on System.Configuration with
      Microsoft.Extension.Configuration
    * Replaced class EnvironmentSection with new class EnvironmentSettings
    * Replaced class FhirEnvironment with new class EnvironmentSettings
    * Removed System.IO.Compression and cleaned up unused code dependent on
      this assembly
    * Removed WindowsBase dependency

---
+++ src/fhir-tool-core/Configuration/EnvironmentSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace FhirTool.Core.Configuration
+{
+    public class EnvironmentSettings
+    {
+        public string Name { get; set; }
+        public string ProxyBaseUrl { get; set; }
+        public string FhirBaseUrl { get; set; }
+    }
+}
--- src/fhir-tool-core/SerializationUtility.cs
+++ src/fhir-tool-core/SerializationUtility.cs
@@ -8,7 +8,6 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Xml.Linq;
 using Tasks = System.Threading.Tasks;
@@ -145,32 +144,6 @@ namespace FhirTool.Core
 
             string data = File.ReadAllText(filename);
             return ImportData(data);
-        }
-
-        public static IEnumerable<string> ExtractZipEntries(this byte[] buffer)
-        {
-            using (Stream stream = new MemoryStream(buffer))
-            using (ZipArchive archive = new ZipArchive(stream, ZipArchiveMode.Read))
-            {
-                foreach (ZipArchiveEntry entry in archive.Entries)
-                {
-                    StreamReader reader = new StreamReader(entry.Open());
-                    string data = reader.ReadToEnd();
-                    yield return data;
-                }
-            }
-        }
-
-        public static IEnumerable<Resource> ExtractResourcesFromZip(this byte[] buffer)
-        {
-            return buffer.ExtractZipEntries().SelectMany(ImportData);
-        }
-
-        public static IEnumerable<Resource> ImportZip(string filename)
-        {
-            EnsureArg.IsNotNullOrWhiteSpace(filename, nameof(filename));
-
-            return File.ReadAllBytes(filename).ExtractZipEntries().SelectMany(ImportData); ;
         }
     }
 }
