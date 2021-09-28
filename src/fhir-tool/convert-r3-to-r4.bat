65700ebd07773fde411e16d73049991ca1fe2570
Kenneth Myhra <kennethmyhra@gmail.com>
2021-09-28 12:15:06 +0200

    Add convert-r3-to-r4.bat
    This adds a new batch file for helping us with conversion between FHIR
    R3 to R4.
    
    There are three input parameters:
    1. The file format you want your output file in, Json or Xml
    2. The file name of the input file (in R3 format)
    3. The file name of output file

---
+++ src/fhir-tool/convert-r3-to-r4.bat
@@ -0,0 +1,6 @@
+﻿# Description of the three input parameters:
+# 1. (%1) The file format you want your output file in, Json or Xml
+# 2. (%3) The file name of the input file (in R3 format)
+# 3. (%2) The file name of output file
+
+.\fhir-tool convert --from R3 --to R4 --format %1 --out %3 %2
